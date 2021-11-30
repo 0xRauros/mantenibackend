ff257d1dc4a1e18b02465fd439928b70ca4ab8e9
Kevin Malla <kmalla@rae.aesl>
2021-11-30 16:15:09 +0100

    forceConsistentCasingInFileNames:false

--- src/routes/UT_Preventivo/utpreventivoRoutes.ts
+++ src/routes/UT_Preventivo/utpreventivoRoutes.ts
@@ -12,18 +12,6 @@ class UTPreventivosRoutes{
         this.router.post('/', utpreventivoController.addUtPreventivo);
         this.router.get('/comprobarfin/:utprevid', utpreventivoController.comprobarOTFin)
         this.router.put('/:utprevid', utpreventivoController.updatePreventivo)
-        // this.router.get('/', utpreventivoController.selectAllUtPreventivos);
-        // this.router.get('/planta/:planta', utpreventivoController.selectUtPreventivosPorPlanta);
-        // this.router.get('/planta/:planta/:area', utpreventivoController.selectUtPreventivosPorArea);
-        // this.router.get('/planta/:planta/:area/:zona', utpreventivoController.selectUtPreventivosPorZona);
-        // this.router.get('/planta/:planta/:area/:zona/:seccion', utpreventivoController.selectUtPreventivosPorSeccion);
-        // this.router.get('/planta/:planta/:area/:zona/:seccion/:codigo', utpreventivoController.selectUtPreventivosPorCodigo);
-        // this.router.get('/planta/:planta/:area/:zona/:seccion/:codigo/:grupo', utpreventivoController.selectUtPreventivosPorGrupo);
-        // this.router.get('/planta/:planta/:area/:zona/:seccion/:codigo/:grupo/:equipo', utpreventivoController.selectUtPreventivosPorEquipo);
-        // this.router.get('/preventivo/:preventivoid', utpreventivoController.selectUtPreventivosPorPreventivo);
-        // this.router.post('/', utpreventivoController.addUtPreventivo);
-        // this.router.put('/:utprevid', utpreventivoController.updatePreventivo);
-        // this.router.delete('/:utprevid', utpreventivoController.deleteUtPreventivo);
     }
 }
 const utpreventivosRoutes = new UTPreventivosRoutes();
