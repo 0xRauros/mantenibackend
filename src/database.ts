46f3368c6016d0c91c495c45afd3d043f517fc36
Rauros <70281191+0xRauros@users.noreply.github.com>
2022-10-18 09:17:49 +0200

    No user/pass display

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,6 @@
 const sql = require('mssql');
 try {
-    sql.connect('mssql://ruben:Windows2018@10.73.80.4/Mantenimiento?encrypt=true');
+    sql.connect('mssql://user:pass@10.73.80.4/Mantenimiento?encrypt=true');
     console.log('DB is connected');
 } catch (err) {
     console.log(err)
